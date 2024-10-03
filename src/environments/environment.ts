f5e84dfb28d83652ce9953259e629fc76fa509af
Matheus Borba <matheus-depaula@hotmail.com>
2024-10-03 01:46:20 -0300

    feat(Firebase): add firebase auth for login and registry

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCnNBErC531q1yJ-p8L3vwDsf1CcQtLPxc",
+        authDomain: "oee-data-collector.firebaseapp.com",
+        projectId: "oee-data-collector",
+        storageBucket: "oee-data-collector.appspot.com",
+        messagingSenderId: "603265353686",
+        appId: "1:603265353686:web:b7080925a93583942b0677"
+    }
+};
